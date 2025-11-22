d7bb5c6f561898ef327e580537cbb07a85888cab
vaelixd <vaelixd@proton.me>
2025-11-22 13:19:58 +1300

    update CSV conversion to work with SCVS v1.0.0

--- convert.go
+++ convert.go
@@ -7,7 +7,17 @@ func AsCSV(parsedSCSV KeyValuePairs) string {
 
 	for key, values := range parsedSCSV {
 		for _, value := range values {
-			conversion += fmt.Sprintf("%s,%s\n", key, value)
+			csvValue := ""
+
+			for idx, column := range value {
+				csvValue += column
+
+				if idx != len(value)-1 {
+					csvValue += ","
+				}
+			}
+
+			conversion += fmt.Sprintf("%s,%s\n", key, csvValue)
 		}
 	}
 
@@ -15,6 +25,8 @@ func AsCSV(parsedSCSV KeyValuePairs) string {
 }
 
 func AsJSON(parsedSCSV KeyValuePairs) string {
+	return "JSON conversions are incorrect since SCSV v1.0.0, they will be fixed with v1.0.1"
+
 	conversion := "{\n"
 	keyIdx := 0
 
@@ -44,6 +56,8 @@ func AsJSON(parsedSCSV KeyValuePairs) string {
 }
 
 func AsYAML(parsedSCSV KeyValuePairs) string {
+	return "YAML conversions are incorrect since SCSV v1.0.0, they will be fixed with v1.0.1"
+
 	conversion := ""
 
 	for key, values := range parsedSCSV {
